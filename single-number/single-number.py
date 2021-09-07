cfa65dd60119d4cb9dcd5978b5bbbc34829c484f
Vaishnavi3799 <66864027+Vaishnavi3799@users.noreply.github.com>
2021-09-07 01:17:28 -0400

    Time: 124 ms (93.77%), Space: 16.8 MB (19.66%) - LeetHub

---
+++ single-number/single-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        counts={}
+        for num in nums:
+            if num not in counts:
+                counts[num]=1 
+            else:
+                del counts[num]
+                
+                
+        return list(counts)[0]
+                
