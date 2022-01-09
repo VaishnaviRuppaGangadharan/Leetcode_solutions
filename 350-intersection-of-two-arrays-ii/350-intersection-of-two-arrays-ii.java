abe796b1cb3e0d74a2f62383077fe931b08aca01
Vaishnavi3799 <66864027+Vaishnavi3799@users.noreply.github.com>
2022-01-09 18:44:11 -0500

    Time: 8 ms (10.65%), Space: 41.2 MB (6.59%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap <Integer, Integer> numsmap1 = new HashMap<> ();
+        HashMap <Integer, Integer> numsmap2 = new HashMap<> ();
+        List<Integer> Output = new ArrayList<>();
+        
+        
+        for(int i=0; i<nums1.length; i++){
+            if(numsmap1.containsKey(nums1[i])){
+                numsmap1.put(nums1[i], numsmap1.get(nums1[i])+1);
+            }
+            else{
+                numsmap1.put(nums1[i], 1);
+            }
+        }
+        
+        for(int i=0; i<nums2.length; i++){
+            if(numsmap2.containsKey(nums2[i])){
+                numsmap2.put(nums2[i], numsmap2.get(nums2[i])+1);
+            }
+            else{
+                numsmap2.put(nums2[i], 1);
+            }
+        }
+        
+        for(int key: numsmap1.keySet()){
+            if(numsmap2.containsKey(key)){
+                int min= Math.min(numsmap1.get(key), numsmap2.get(key));
+                for(int i=0; i<min; i++){
+                    Output.add(key);
+                }}
+            else{
+                numsmap2.remove(key);
+            }
+            }
+        int[] outputArray = new int[Output.size()];
+        for(int i=0; i<Output.size(); i++){
+            outputArray[i]=Output.get(i);
+        }
+         
+        return outputArray; 
+}
+}
