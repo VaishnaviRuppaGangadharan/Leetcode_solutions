6faf95fecfebc2497da43285eadfa6271827319b
Vaishnavi3799 <66864027+Vaishnavi3799@users.noreply.github.com>
2022-01-11 13:49:03 -0500

    Time: 1 ms (32.01%), Space: 38.6 MB (11.98%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,7 +1,9 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+        
         List<List<Integer>> triangle = new ArrayList<>();
         triangle.add(new ArrayList<>());
+        
         triangle.get(0).add(1);
         
         for(int i=1; i<numRows; i++){
@@ -11,7 +13,6 @@ class Solution {
             for(int j=1; j<prevrow.size(); j++){
                 currRow.add(prevrow.get(j -1) + prevrow.get(j));
             }
-        
         
             currRow.add(1);
             triangle.add(currRow);
