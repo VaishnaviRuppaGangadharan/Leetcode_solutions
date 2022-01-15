12b4bc01ec9a6730d885ca0e7a208d95c6ec2a8b
Vaishnavi3799 <66864027+Vaishnavi3799@users.noreply.github.com>
2022-01-15 18:10:17 -0500

    Time: 2 ms (50.58%), Space: 38.9 MB (15.64%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> myStack = new Stack();
+        for(char c: s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                myStack.push(c);
+            }
+            else if(c == ')' && !myStack.isEmpty() && myStack.peek() == '('){
+                myStack.pop();
+            }
+             else if(c == '}' && !myStack.isEmpty() && myStack.peek() == '{'){
+                myStack.pop();
+            }
+             else if(c == ']' && !myStack.isEmpty() && myStack.peek() == '['){
+                myStack.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return myStack.isEmpty();
+        
+    }
+}
