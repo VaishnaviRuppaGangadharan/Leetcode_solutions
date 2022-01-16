7dfe5d2bfd555b99138bbf66fabb8dd8b2e99b9a
Vaishnavi3799 <66864027+Vaishnavi3799@users.noreply.github.com>
2022-01-16 18:30:24 -0500

    Time: 3 ms (58.81%), Space: 39.8 MB (24.73%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if(s.length() == 0) {
+            return 0;
+        }
+        long num = 0;
+        
+        boolean isNegative = s.charAt(0) == '-' ? true : false;
+        
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(i == 0 && (c == '+' || c == '-')) {
+                continue;
+            }
+            if(Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+                if(!isNegative && num >= Integer.MAX_VALUE) {
+                    return Integer.MAX_VALUE;
+                }
+                else if(isNegative && num >= (long) Integer.MAX_VALUE + 1) {
+                    return Integer.MIN_VALUE;
+                }
+            }
+            else{
+                break;
+            }
+        }
+        
+        int res = (int) num;
+        return isNegative ? -res : res;
+    }
+}
