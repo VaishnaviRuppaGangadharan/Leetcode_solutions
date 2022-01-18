28b0288f45879356dcf1951340dadb541c19669d
Vaishnavi3799 <66864027+Vaishnavi3799@users.noreply.github.com>
2022-01-17 22:03:49 -0500

    Time: 11 ms (37.27%), Space: 38.7 MB (43.55%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String countAndSay(int n) {
+        String result = "1";
+        while(n > 1){
+            StringBuilder str = new StringBuilder();
+            for(int i=0; i<result.length(); ++i){
+                int count = 1;
+                while(i+1 < result.length() && result.charAt(i)==result.charAt(i+1)){
+                    ++count;
+                    ++i;
+                }
+                str.append(count).append(result.charAt(i));
+                }
+            result = str.toString();
+            --n;
+    }
+        return result;
+    }
+}
