5dca415b4efd1ff503723c938522295658e16275
Vaishnavi3799 <66864027+Vaishnavi3799@users.noreply.github.com>
2022-02-11 22:42:57 -0500

    Time: 0 ms (100.00%), Space: 42.2 MB (11.30%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -14,24 +14,30 @@
  * }
  */
 class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
-        LinkedList<TreeNode> stack = new LinkedList<>();
-        LinkedList<Integer> output = new LinkedList<>();
-        if(root == null){
-            return output;
-        }
-        stack.add(root);
-        while(!stack.isEmpty()){
-            TreeNode node = stack.pollLast();
-            output.add(node.val);
+        if(root == null) return list;
+        list.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return list;
+//         LinkedList<TreeNode> stack = new LinkedList<>();
+//         LinkedList<Integer> output = new LinkedList<>();
+//         if(root == null){
+//             return output;
+//         }
+//         stack.add(root);
+//         while(!stack.isEmpty()){
+//             TreeNode node = stack.pollLast();
+//             output.add(node.val);
             
-            if(node.right!=null){
-                stack.add(node.right);
-            }
-            if(node.left!=null){
-                stack.add(node.left);
-            }
-        }
-        return output;
-    }
+//             if(node.right!=null){
+//                 stack.add(node.right);
+//             }
+//             if(node.left!=null){
+//                 stack.add(node.left);
+//             }
+//         }
+//         return output;
+     }
 }
