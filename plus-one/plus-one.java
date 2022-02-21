cc695c25ca6c1973222c5967a7e0a6040d103c68
Vaishnavi3799 <66864027+Vaishnavi3799@users.noreply.github.com>
2022-02-21 17:38:36 -0500

    Time: 0 ms, Memory: 42.9 MB - LeetHub

--- plus-one/plus-one.java
+++ plus-one/plus-one.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int lastDigit = digits.length - 1;
-        for(int i=lastDigit; i>=0; --i){
+        for(int i=lastDigit; i>=0; i--){
             if(digits[i]==9){
                 digits[i]=0; 
             }
