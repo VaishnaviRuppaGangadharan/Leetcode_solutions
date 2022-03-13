3bc747a6edc1a67477fe590414d463700410f8a4
Vaishnavi3799 <66864027+Vaishnavi3799@users.noreply.github.com>
2022-03-13 01:02:09 -0500

    Time: 3 ms (62.31%), Space: 56.1 MB (40.32%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,21 +1,40 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int ans = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int n = nums.length;
+//         int ans = 0;
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for(int num: nums){
+//             if(!map.containsKey(num)){
+//                 map.put(num, 1);
+//             }    
+//             else{
+//                 map.put(num, map.get(num) + 1);
+//             }
+//             if(map.get(num) > (n/2))
+//                {
+//                    ans = num;
+//                     break;
+//                }           
+//     }
+//         return ans;
+//     }
+// }
+
+class Solution{
+    public int majorityElement(int[] nums){
+        int ct = 0;
+        Integer major_ele = null;
         for(int num: nums){
-            if(!map.containsKey(num)){
-                map.put(num, 1);
-            }    
+            if(ct == 0)
+                major_ele = num;
+            if(major_ele == num){
+                ct+=1;
+            } 
             else{
-                map.put(num, map.get(num) + 1);
+                ct-=1;
             }
-            if(map.get(num) > (n/2))
-               {
-                   ans = num;
-                    break;
-               }           
-    }
-        return ans;
+                   
+        }
+        return major_ele;
     }
 }
