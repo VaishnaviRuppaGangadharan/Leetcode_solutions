d9bdf30dcc210d41922d0c0390242e281bb6e53e
Vaishnavi3799 <66864027+Vaishnavi3799@users.noreply.github.com>
2022-03-15 17:56:15 -0400

    Time: 441 ms (21.88%), Space: 150.6 MB (6.81%) - LeetHub

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public static List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>>result = new ArrayList<>();
+		Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++) {
+        	if((i == 0) || (nums[i] != nums[i-1])) {
+        		findComp(nums, i, result);
+        	}
+        }
+        return result;
+    }
+	public static void findComp(int[] nums, int i, List<List<Integer>> result) {
+        HashSet<Integer> seen = new HashSet<>();
+		for(int j=i+1; j<nums.length; j++) {
+			int comp = -nums[i]-nums[j];
+			if(seen.contains(comp)) {
+				result.add(Arrays.asList(nums[i],nums[j], comp));
+				while (j + 1 < nums.length && nums[j] == nums[j + 1])
+                    ++j;
+			}
+			seen.add(nums[j]);	
+			
+		}
+		
+	}  
+    
+}
